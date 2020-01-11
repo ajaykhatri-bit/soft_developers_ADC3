6fc2fae99ed663d3d955b57a1a0a86ac9b942ec8
ajaykhatri-bit <ajaykhatri-bit@users.noreply.github.com>
2020-01-11 11:11:46 +0545

    Update settings.py

--- webdevelopmentstore/webdevelopmentstore/settings.py
+++ webdevelopmentstore/webdevelopmentstore/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'software.apps.SoftwareConfig',
+    'software',
+
 ]
 
 MIDDLEWARE = [
