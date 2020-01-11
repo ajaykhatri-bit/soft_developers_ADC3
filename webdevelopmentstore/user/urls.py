a64053b112453f728e2571b8094431c2e91cca75
ajaykhatri-bit <ajaykhatri-bit@users.noreply.github.com>
2020-01-11 22:36:55 +0545

    Update urls.py

--- webdevelopmentstore/user/urls.py
+++ webdevelopmentstore/user/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
 	path('register/', views.register, name="register"), 
 	path('logout/', views.user_logout, name="logout"),
 	path('login/', views.user_login, name="login"),
+
+	#path('', views.register,name='register_insert'), # get and post req. for insert operation
+    #path('<int:id>/', views.register,name='register_update'), # get and post req. for update operation
+    #path('delete/<int:id>/',views.register_delete,name='register_delete'),
+    #path('list/',views.registerlist,name='registerlist') # get req. to retrieve and display all records
+
 ]
