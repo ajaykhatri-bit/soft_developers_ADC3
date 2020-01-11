ec142c31136ffb138a98cbdf620e3bf01176d112
ajaykhatri-bit <ajaykhatri-bit@users.noreply.github.com>
2020-01-11 09:28:39 +0545

    Update views.py

--- webdevelopmentstore/software/views.py
+++ webdevelopmentstore/software/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 from .models import App
 
 # Create your views here.
-def App(request):
-	return render(request=request, template_name="main/home.html", context={"apps": App.objects.all})
+#def App(request):
 	#return HttpResponse("we are in development phase")
 
-#def homepage(request):
-	#return render(request=request, template_name="main/home.html", context={"apps": App.objects.all})
+def homepage(request):
+	return render(request=request, template_name="main/home.html", context={"apps": App.objects.all})
