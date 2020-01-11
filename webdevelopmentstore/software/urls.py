cb11de246c942391d3b35fdfc8fa513ea5347bc1
ajaykhatri-bit <ajaykhatri-bit@users.noreply.github.com>
2020-01-11 11:11:31 +0545

    Update urls.py

--- webdevelopmentstore/software/urls.py
+++ webdevelopmentstore/software/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 from .import views
 
 urlpatterns = [
-	path('', include('main.urls')),
-    path('admin/', admin.site.urls),
     path('', views.homepage, name = "homepage"),
+	
+
 ]
