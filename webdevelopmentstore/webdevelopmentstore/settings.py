a96dc143a72bf36b0452223a8ab06b91942e60f1
ajaykhatri-bit <ajaykhatri-bit@users.noreply.github.com>
2020-01-25 13:24:59 +0545

    Update settings.py

--- webdevelopmentstore/webdevelopmentstore/settings.py
+++ webdevelopmentstore/webdevelopmentstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'software',
     'user',
+    'download',
     #'crispy_forms'
 
 ]
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'webdevelopmentstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
