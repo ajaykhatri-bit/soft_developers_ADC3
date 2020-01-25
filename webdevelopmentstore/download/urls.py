154fda00fc4932a44176bc44997b9cc4296fa707
ajaykhatri-bit <ajaykhatri-bit@users.noreply.github.com>
2020-01-25 13:22:31 +0545

    Update urls.py

--- webdevelopmentstore/download/urls.py
+++ webdevelopmentstore/download/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 from .import views
 
+app_name = "download"
 urlpatterns = [
- path('', views.homepage, name="homepage"),
+ path('download/', views.download, name = "download"),
+ path('download/upload/',views.upload_stw, name="upload"),
+ path('download/soft_list', views.soft_list, name="soft_list"),
+ path('download/<int:pk/>', views.delete_stw,name="delete_stw"),
+
+ path("api/", views.api_data, name="api_data"),
+ path('change/<int:pk>', views.update_api_data, name="update_api_data")
  ]
